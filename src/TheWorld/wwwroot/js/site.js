4104bd9ff046102f674b789e39a065de35b8ce64
sumant30 <sumant30@gmail.com>
2017-02-08 07:26:55 +0530

    Integrated bootstrap

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -3,15 +3,18 @@
 (function () {
 
     var $sidebarandWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa")
 
     $("#sidebarToggle").on("click", function () {
         $sidebarandWrapper.toggleClass("hide-sidebar");
         if ($sidebarandWrapper.hasClass("hide-sidebar")) {
-            $("#sidebarToggle").val("Show-Sidebar")
+            $icon.removeClass("fa fa-angle-left");
+            $icon.addClass("fa fa-angle-right");
         }
         else {
-            $("#sidebarToggle").val("Hide-Sidebar")
-        }
+            $icon.removeClass("fa fa-angle-right");
+            $icon.addClass("fa fa-angle-left");
+         }
     });
     
 
