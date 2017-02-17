1942b178a859a436ac2fbae5f761a744b59b7fe7
sumant30 <sumant30@gmail.com>
2017-02-17 07:26:06 +0530

    Added get+post in angular 1 along with a directive

--- src/TheWorld/wwwroot/js/tripsController.js
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -3,35 +3,53 @@
     //Getting the module
     angular.module("app-trips").controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
 
         var vm = this;
 
-        vm.trips =
-            [
-                {
-                    name: "US Trip",
-                    created: new Date()
-                },
-                {
-                    name: "World Trip",
-                    created: new Date()
-                }
-            ];
+        vm.trips = [];
 
         vm.newTrip = {};
 
+        vm.errorMessage = '';
+
+        vm.isBusy = true;
+
+        $http.get('/api/trips')
+            .then(
+                    function (response) {
+                        //Success
+                        angular.copy(response.data, vm.trips);
+                    },
+                    function (error) {
+                        //Failure
+                        vm.errorMessage = 'Failed to get trips : ' + error;
+                    }
+            ).finally(function () {
+                vm.isBusy = false;
+            });
+
         vm.addTrip = function () {
 
-            vm.trips.push(
-                    {
-                        name: vm.newTrip.name,
-                        created: new Date()
-                    }
-                );
+            vm.isBusy = true;
 
-            vm.newTrip = {};
-            
+            vm.errorMessage = '';
+
+            $http.post('/api/trips',vm.newTrip)
+            .then(
+                    function (response) {
+                        //Success
+                        vm.trips.push(response.data);
+                        vm.newTrip = {};
+                    },
+                    function (error) {
+                        //Failure
+                        vm.errorMessage = 'Failed to post trips : ' + error;
+                    }
+            ).finally(function () {
+                vm.isBusy = false;
+            });
+
         }
     }
 })();
